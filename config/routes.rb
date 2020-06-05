e7204375cbb119c63f3b6bbfea536c614f15b88e
Jon Murphy <twistir123@gmail.com>
2020-06-05 13:30:30 -0400

    Updated routes for blogs, page-home, page-contact,page-about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  resources :blogs  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
